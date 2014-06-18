2ae5ed8d5a55e0dff0fce0145bd92bca0bb1266b
Yuya Adachi <yuya_adachi@3ca.co.jp>
2014-06-18 18:50:27 +0900

    modify some bug ... however there didn't still fix all

--- js/main.js
+++ js/main.js
@@ -44,6 +44,10 @@ window.onload = function(){
 	var touch_pos_x = 0;
 	var touch_pos_y = 0;
 
+	var dColArray = new Array();
+	var dXArray = new Array();
+	var dYArray = new Array();
+
     var game = new Core(320, 320);
     game.fps = 60;
 	game.preload('image/yamochi.png');
@@ -59,10 +63,13 @@ window.onload = function(){
 
 		left_sprite.image = surface;
 		left_sprite.position = { x : 0, y : 0 };
+		left_sprite.kind = "wall";
 		right_sprite.image = surface;
 		right_sprite.position = { x : game.width - 1, y : 0 };
+		right_sprite.kind = "wall";
 		top_sprite.image = surface;
 		top_sprite.position = { x : 0, y : 0 };
+		top_sprite.kind = "wall";
 
 		_scene.addChild(left_sprite);
 		_scene.addChild(right_sprite);
@@ -71,7 +78,7 @@ window.onload = function(){
 
 	function buildPlayerBlock(/*_posX, _posY */ _width, _height, _pad, _world){
 		var surface = new Surface( _width, _height);
-		var sprite = new PhyBoxSprite( _width, _height, enchant.box2d.STATIC_SPRITE, 1.0, 0.3, 1.0, true);
+		var sprite = new PhyBoxSprite( _width, _height, enchant.box2d.STATIC_SPRITE, 1.0, 0.8, 0.3, true);
 		var s_x = Math.floor(game.width/2) - Math.floor(surface.width/2);
 		var s_y = Math.floor(game.height) - Math.floor(surface.height) - _pad.height;
 
@@ -80,6 +87,8 @@ window.onload = function(){
 
 		sprite.image = surface;
 		sprite.position = { x : s_x, y : s_y };
+		sprite.kind = "player";
+
 
 		console.log("player x = " + s_x + " player y = " + s_y);
 
@@ -91,22 +100,48 @@ window.onload = function(){
 	var boundRatio = game.fps * 2;
 	var isFirstTime = true;
 
-	function isDestroyBlock(_target, _block){
+	function destroyBlock(_target, _block, _player, _pad, _scene, _world){
 		var ii;
-		var isBroken = false;
 		for(ii=0; ii<_block.length; ii++){
 			var tmp_block = _block[ii];
 			if(_target === tmp_block){
+				dColArray.push(tmp_block.color);
+				dXArray.push(tmp_block.x);
+				dYArray.push(tmp_block.y);
 				tmp_block.destroy();
-				isBroken = true;
 			}
 		}
-		return isBroken;
 	}
 
-	function buildBall(_player, _block, _color, _rad, _pad, _scene, _world){
+	function setPlayerCallback(_player,_ball){
+		_player.addEventListener(Event.TOUCH_START, function(e) {
+			touch_pos_x = e.x;
+			touch_pos_y = e.y;
+		});
+
+		_player.addEventListener(Event.TOUCH_MOVE, function(e) {
+			var tmp_x = e.x;
+			var tmp_y = e.y;
+			var dest_x = touch_pos_x - tmp_x;
+
+			_player.x -= dest_x;
+
+			if(_ball.isAwake == false){
+				_ball.x -= dest_x;
+			}
+
+			if(_player.x >= game.width - _ball.width - 5){
+				_player.x = game.width - _ball.width - 5;
+			}
+			else if(_player.x <= 0 + 5){
+				_player.x = 5;
+			}
+		});
+	}
+
+	function buildBall(_player, _color, _rad, _x, _y){
 		var surface = new Surface( _rad*2, _rad*2);
-		var sprite = new PhyCircleSprite(_rad, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.2, 1.0, true);
+		var sprite = new PhyCircleSprite(_rad, enchant.box2d.DYNAMIC_SPRITE, 1.0, 0.2, 0.2, false);
 		var s_x;
 		var s_y;
 
@@ -115,20 +150,89 @@ window.onload = function(){
 		surface.context.arc(_rad, _rad, _rad, 0, Math.PI*2, false);
 		surface.context.fill();
 
-		s_x = Math.floor(_player.x) + Math.floor(_player.width/2) - Math.floor(surface.width/2);
-		s_y = Math.floor(_player.y) - Math.floor(surface.height);
+		if(_x == undefined){
+			s_x = Math.floor(_player.x) + Math.floor(_player.width/2) - Math.floor(surface.width/2);
+		}
+		else{
+			s_x = _x;
+		}
+		if(_y == undefined){
+			//s_y = Math.floor(_player.y) - Math.floor(surface.height);
+			s_y = 150;
+		}
+		else{
+			s_y = _y;
+		}
 
 		console.log("ball x = " + s_x + " ball y = " + s_y);
 
 //		sprite.image = surface;
         sprite.image = game.assets['image/yamochi.png'];
 		sprite.frame = 0;
+		sprite.position = { x : s_x, y : s_y };
 		sprite.isAwake = true;
-		sprite.position = { x : s_x, y : 150};
-
+		sprite.kind = "ball";
 
 		return sprite;
 	};
+
+	function setBallCallback(_ball, _world) {
+
+		_ball.addEventListener(Event.ENTER_FRAME, function(e){
+//			var moveX = this.x;
+//			var moveY = this.y;
+//			var ii;
+//
+			_world.step(game.fps);
+//
+//			if(this.x >= game.width){
+//				_ball.applyImpulse( new b2Vec2(-0.5, -0.3) )
+//			}
+//			else if(this.x <= 0){
+//				_ball.applyImpulse( new b2Vec2(0.5, -0.3) )
+//			}
+//
+//			if(this.y <= 0){
+//			}
+//			else if(this.y >= game.height){
+//				_ball.applyImpulse( new b2Vec2(0.0, -50.0) )
+//			}
+//
+//			_ball.contact(function(obj){
+//				if(obj.kind == "player"){
+//					if(upPower == true){
+//						upPower = false;
+//						downPower = false;
+//						_ball.applyImpulse( new b2Vec2(0.0, -10.0) );
+//						console.log("upPower");
+//						_ball.isAwake = true;
+//					}
+//					else if(downPower == true){
+//						upPower = false;
+//						downPower = false;
+//						console.log("downPower");
+//						_ball.isAwake = false;
+//					}
+//					_ball.setAwake(_ball.isAwake);
+//				}
+//				else if(obj.kind == "block") {
+//					destroyBlock(obj, _block, _player, _pad, _scene, _world);
+//				}
+//			});
+		});
+	}
+
+	function divideBall(_player, _scene, _world, _x, _y){
+		var ball = buildBall(_player, "red", 50/2, _x, _y); 
+		ball.setAwake(true);
+		setBallCallback(ball, _world);
+		_scene.addChild(ball);
+
+		// terminated bug 
+		//red_ball.push(ball);
+	}
+
+	var sBlockGroup;
 
 	function buildBlocks(_scene, _lines, _player, _pad, _world){
 		var s_w = 30;
@@ -138,6 +242,8 @@ window.onload = function(){
 		var sprite = Array();
 		var ii, jj, cnt;
 		var s_x, s_y;
+
+		sBlockGroup = new Group();
 
 		cnt = 0;
 		for(ii=0; ii<game.width; ii+=s_w){
@@ -163,144 +269,135 @@ window.onload = function(){
 
 				sprite[cnt] = new PhyBoxSprite( s_w, s_h,
 												enchant.box2d.STATIC_SPRITE,
-												0.3, 0.2, 0.6, true);
+												1.0, 0.3, 0.8, true);
 				sprite[cnt].image = surface;
 				sprite[cnt].color = surface.context.fillStyle;
 				sprite[cnt].position = { x : ii, y : jj };
+				sprite[cnt].kind = "block";
 
-				_scene.addChild(sprite[cnt]);
+//				_scene.addChild(sprite[cnt]);
+				sBlockGroup.addChild(sprite[cnt]);
 				cnt++;
 			}
 		}
+		_scene.addChild(sBlockGroup);
+
+		sBlockGroup.addEventListener(Event.CHILD_REMOVED, function(e){
+			var col = dColArray.pop();
+			var x = dXArray.pop();
+			var y = dYArray.pop();
+			if(col == "#0000ff"){
+				divideBall(_player, _scene, _world, x, y);
+			}
+		});
+
 		return sprite;
 	};
 
+	var red_ball = [];
 	var startScene = function (){
 		var world = new PhysicsWorld( 0.0, 9.8 );
 		var scene = new Scene();
 		var pad = new Pad();
 
-		var red_ball = [];
+		var count = 0;
 		var ii = 0;
 
 		var player = buildPlayerBlock(240, 12, pad, scene, world);
 		var block = buildBlocks(scene, 10, player, pad, world);
 
 		for(ii=0; ii<1; ii++){
-			red_ball[ii] = buildBall(player, block, "red", 50/2, pad, scene, world); 
-			red_ball[ii].isAwake = true;
-			scene.addChild(red_ball[ii]);
+			red_ball[count] = buildBall(player, "red", 50/2); 
+			red_ball[count].isAwake = true;
+			red_ball[count].setAwake(red_ball[count].isAwake);
+			scene.addChild(red_ball[count]);
 
-			red_ball[ii].addEventListener(Event.ENTER_FRAME, function(e){
-				var moveX = this.x;
-				var moveY = this.y;
-				var ii;
+			setPlayerCallback(player, red_ball);
+			setBallCallback(red_ball[count], world);
+			count++;
+		}
+		buildWall(scene, world);
 
-				world.step(game.fps);
-				if(this.x >= game.width){
-					red_ball[ii].applyImpulse( new b2Vec2(-0.5, -0.3) )
-				}
-				else if(this.x <= 0){
-					red_ball[ii].applyImpulse( new b2Vec2(0.5, -0.3) )
-				}
+		scene.addChild(player);
 
-				if(this.y <= 0){
-				}
-				else if(this.y >= game.height){
-					red_ball[ii].applyImpulse( new b2Vec2(0.0, -50.0) )
-				}
-
-				red_ball[ii].contact(function(obj){
-					if(obj === _player){
-						if(upPower == true){
-							upPower = false;
-							downPower = false;
-							red_ball[ii].applyImpulse( new b2Vec2(0.0, -10.0) );
-							console.log("upPower");
-							red_ball[ii].setAwake(true);
-							red_ball[ii].isAwake = true;
-						}
-						else if(downPower == true){
-							upPower = false;
-							downPower = false;
-							console.log("downPower");
-							red_ball[ii].setAwake(false);
-							red_ball[ii].isAwake = false;
-						}
-						else{
-							//sprite.applyImpulse( new b2Vec2(0.0, -0.3) );
-							//sprite.applyTorque(0.3);
-						}	
-					}
-					else{
-						var isBroken = isDestroyBlock(obj, _block);
-						if((isBroken == true) && (obj.color == "#0000ff")){
-							var ball = buildBall(player, red_ball[ii], "red", 50/2, pad, scene, world); 
-							scene.addChild(ball);
-						}
-					}
-				});
-			});
-
-			player.addEventListener(Event.ENTER_FRAME, function(e){
-				var move_distance = player.x;
+		scene.addEventListener(Event.ENTER_FRAME, function(e){
+			var move_distance = player.x;
+			{
+				/* focus on player */
 				var input = game.input;
-
 				if (input.up)    {
 					upPower = true;
 				}
 				if (input.down)  {
 					downPower = true;
 				}
-
+	
 				if (input.left)  {
 					player.x -= 8;
 				}
 				if (input.right) {
 					player.x += 8;
 				}
-
+	
 				if(player.x <= 0) {
 					player.x = 0;
 				}
-
+	
 				if(player.x + player.width > game.width) {
 					player.x = game.width - player.width;
 				}
+			}
 
-				if((red_ball[ii].isAwake == false) && (move_distance != 0)){
-					move_distance -= player.x;
-					red_ball[ii].x -= move_distance;
+			{
+				/* focus on ball */
+				for(var ii=0; ii<red_ball.length; ii++){
+					var ball = red_ball[ii];
+					var moveX = red_ball[ii].x;
+					var moveY = red_ball[ii].y;
+
+					if(ball.isAwake == false){
+						move_distance -= player.x;
+						ball.x -= move_distance;
+					}
+					else{
+						if(ball.x >= game.width){
+							ball.applyImpulse( new b2Vec2(-0.5, -0.3) )
+						}
+						else if(ball.x <= 0){
+							ball.applyImpulse( new b2Vec2(0.5, -0.3) )
+						}
+
+						if(ball.y <= 0){
+						}
+						else if(ball.y >= game.height){
+							ball.applyImpulse( new b2Vec2(0.0, -50.0) )
+						}
+
+						ball.contact(function(obj){
+							if(obj.kind == "player"){
+								if(upPower == true){
+									upPower = false;
+									downPower = false;
+									ball.applyImpulse( new b2Vec2(0.0, -10.0) );
+									console.log("upPower");
+									ball.isAwake = true;
+								}
+								else if(downPower == true){
+									upPower = false;
+									downPower = false;
+									console.log("downPower");
+									ball.isAwake = false;
+								}
+								ball.setAwake(ball.isAwake);
+							}
+							else if(obj.kind == "block") {
+								destroyBlock(obj, block, player, pad, scene, world);
+							}
+						});
+					}
 				}
-			});
-
-			player.addEventListener(Event.TOUCH_START, function(e) {
-				touch_pos_x = e.x;
-				touch_pos_y = e.y;
-			});
-
-			player.addEventListener(Event.TOUCH_MOVE, function(e) {
-				var tmp_x = e.x;
-				var tmp_y = e.y;
-				var dest_x = touch_pos_x - tmp_x;
-
-				player.x -= dest_x;
-
-				if(red_ball[ii].isAwake == false){
-					red_ball[ii].x -= dest_x;
-				}
-
-				if(player.x >= game.width - red_ball[ii].width - 5){
-					player.x = game.width - red_ball[ii].width - 5;
-				}
-				else if(player.x <= 0 + 5){
-					player.x = 5;
-				}
-			});
-		}
-		buildWall(scene, world);
-
-		scene.addChild(player);
+			}
+		});
 
 		pad.moveTo(0, game.height-pad.height);
 		scene.addChild(pad);
